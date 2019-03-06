39b9e1273ea9b4b9730df14c1e1a091703930429
TianRB <tianrb@gmail.com>
2019-03-06 11:53:44 -0600

    Initial commit

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class UserSeeder extends Seeder
+{
+	/**
+	* Run the database seeds.
+	*
+	* @return void
+	*/
+	public function run()
+	{
+		DB::table('users')->insert([
+			'name' => 'Admin',
+			'email' => 'admin'.'@admin.com',
+			'password' => bcrypt('secret'),
+			'created_at' => Carbon::now(),
+			'updated_at' => Carbon::now()
+		]);
+	}
+}
